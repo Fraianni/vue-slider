92de2c667e3817caa3ac05a1ca9993a52e71d5f4
Fraianni <francescoianni1995@gmail.com>
2022-09-13 15:38:31 +0200

    funzionamento principale migliorato

--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
 
-console.log(slides);
+
 
 const app = new Vue({
     el: '#app',
     data: {
-        order: 1
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
+        order: 0
     },
     methods: {
         previousImg: function () {
-            if (this.order === 1)
-                this.order = 5;
+            if (this.order === 0)
+                this.order = this.slides.length - 1;
             else
                 this.order--;
         },
 
         nextImg: function () {
-            console.log(this.order)
-            if (this.order === 5)
-                this.order = 1;
+            if (this.order === this.slides.length - 1)
+                this.order = 0;
             else
                 this.order++;
+
+            console.log(this.order)
+
         }
 
     }
